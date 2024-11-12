a3ed61a4be1c334a7c7d40c04249a60d6d66f077
EmreKrdltru <emrekardal@trabzon.edu.tr>
2024-11-12 14:24:50 +0300

    .

--- PublicationParse/DTOs.cs
+++ PublicationParse/DTOs.cs
@@ -2,18 +2,6 @@
 
 namespace PublicationParse
 {
-    public class AktifYazarModel
-    {
-        public int Id { get; set; }
-        public int PersonId { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string CorporateInformation { get; set; }
-        public bool IsEmployee { get; set; }
-        public string EMail { get; set; }
-        public bool IsActive { get; set; }
-        public string LastAutoUpdateTime { get; set; }
-    }
     public class User
     {
         public int PersonId { get; set; }
